e1481d2f2dd22fd893db1939696375b561dd5925
gabrielecorti <borntofrappe@gmail.com>
2018-08-18 17:25:24 +0200

    include d3.treemap() layout

--- script.js
+++ script.js
@@ -38,3 +38,35 @@ const svgCanvas = svgContainer
   .append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+
+// retrieve the JSON format and pass it in a function responsible to draw the diagram itself
+const URL = "https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/movie-data.json";
+
+fetch(URL)
+  .then((response) => response.json())
+  .then((json) => drawDiagram(json));
+
+function drawDiagram(data) {
+  // log the JSON file
+  // console.log(data);
+
+  /* 
+  d3.treemap() cannot use the JSON format directly
+  it is first necessary to include a root node and values for all the branches, outlining the relative weight of each division
+  d3.hierarchy includes the node
+  node.sum allows to include the value for each data point and branch
+  */
+
+  // include a root node
+  let hierarchy = d3.hierarchy(data);
+
+  // compute the value for each branch, dividing the nodes on the basis of the nodes' values
+  hierarchy.sum((d) => d.value);
+
+  // create a treemap layout
+  let treemap = d3.treemap();
+  
+  // display the data, as modified per the treemap layout 
+  console.log(treemap(hierarchy));
+
+}
